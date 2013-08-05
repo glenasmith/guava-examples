823abf4f8ae34fb06b1c6fc0cd7edb3d4f075e40
Glen <glen@bytecode.com.au>
2013-08-05 16:08:50 +1000

    Added splitting example

--- src/test/java/au/com/bytecode/guava/StringRelatedTimeSavers.java
+++ src/test/java/au/com/bytecode/guava/StringRelatedTimeSavers.java
@@ -1,6 +1,11 @@
 package au.com.bytecode.guava;
 
+import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import java.util.List;
+import java.util.Set;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,6 +21,28 @@ public class StringRelatedTimeSavers {
         assertTrue(Strings.isNullOrEmpty(null));
         assertTrue(Strings.isNullOrEmpty(""));
         assertFalse(Strings.isNullOrEmpty("       "));
+    }
+    
+    @Test
+    public void splittingLikeButter() {
+        
+        List<String> roles = Lists.newArrayList("role1", " role1 , role2 ",
+                null, " ", "role2 ");
+        
+        Splitter mySplitter = Splitter.on(',').trimResults().omitEmptyStrings();
+        Set<String> rolesFound = Sets.newHashSet();
+        for (String nextRole : roles) {
+            if (!Strings.isNullOrEmpty(nextRole)) {
+                for (String eachRoleOnLine : mySplitter.split(nextRole)) {
+                    rolesFound.add(eachRoleOnLine);
+                }
+            }
+        
+        }
+        assertEquals(2, rolesFound.size());
+        assertTrue(rolesFound.contains("role1"));
+        assertTrue(rolesFound.contains("role2"));
+        
     }
     
 }
